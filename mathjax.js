d1fc72b35a447dec92a1378d532d1247a3f031c1
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-03-31 20:22:16 -0300

    quarto render + inicio das anotações aula 8

---
+++ mathjax.js
@@ -0,0 +1,12 @@
+window.MathJax = {
+  loader: {load: ['[tex]/cancel']},
+  tex: {packages: {'[+]': ['cancel']}}
+};
+window.MathJax = {
+  loader: {load: ['[tex]/cancel']},
+  tex: {packages: {'[+]': ['cancelto']}}
+};
+window.MathJax = {
+  loader: {load: ['[tex]/color']},
+  tex: {packages: {'[+]': ['color']}}
+};
